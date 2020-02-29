2e473f24c9cf7221f49aaf842239fd10bcc4f215
DavideVitiello <lgvitiello9@gmail.com>
2020-02-29 17:50:10 +0100

    Niente di che

--- src/Contenuti/Indirizzi.js
+++ src/Contenuti/Indirizzi.js
@@ -1,38 +1,22 @@
-import React, {
-  useEffect,
-  useState
-} from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Indirizzo from "./Indirizzo";
 
 import "../App.css";
 
 function Indirizzi(props) {
   let oggetto = props.dati;
-  return ( <
-    React.Fragment >
-    /{" "} <
-    Indirizzo key = {
-      oggetto.nome
-    }
-    nome = {
-      oggetto.nome
-    }
-    id = {
-      oggetto.id
-    }
-    dati = {
-      props.dati
-    }
-    link = {
-      props.link
-    }
-    />{" "} <
-    /React.Fragment>
+  return (
+    <React.Fragment>
+      /{" "}
+      <Indirizzo
+        key={oggetto.nome}
+        nome={oggetto.nome}
+        id={oggetto.id}
+        dati={props.dati}
+        link={props.link}
+      />{" "}
+    </React.Fragment>
   );
 }
 
