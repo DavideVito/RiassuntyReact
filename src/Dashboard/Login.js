03879828de5ce6bd973e877ee3c4d6bc77211d22
DavideVitiello <lgvitiello9@gmail.com>
2020-03-03 17:01:02 +0100

    Tutto dovrebbe funzionare

--- src/Dashboard/Login.js
+++ src/Dashboard/Login.js
@@ -1,21 +1,20 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import GoogleLogin from "react-google-login";
 import FullNavBar from "../NavBar/FullNavBar";
-import axios from "axios";
 import Bottone from "../Util/Bottone";
 import "../App.css";
 import $ from "jquery";
 import MostraRiassunti from "./MostraRiassunti";
 
 function Login(props) {
-  let [account, impostaAccount] = useState({});
-  let [riassunti, cambiaRiassunti] = useState([]);
-  let [riassuntiDaApprovare, cambiaRiassuntiDaApprovare] = useState([]);
+  let [ok, cambiaOk] = useState(false);
+  let [udid, cambiaUDID] = useState();
 
   const esisteGia = () => {
+    if (udid === 0 || typeof udid === "undefined") {
+      return;
+    }
     async function controllaCheEsistaGia() {
-      let udid = "109232597291200925390";
       let data = new FormData();
       data.append("id", udid);
       let token = sessionStorage.token;
@@ -30,48 +29,77 @@ function Login(props) {
           body: data
         }
       );
-
       risposta = await risposta.json();
       sessionStorage.token = risposta.token;
       console.log(risposta);
-      $("#loadingImage").fadeOut(500, "swing");
+      cambiaOk(true);
     }
 
     controllaCheEsistaGia();
   };
 
-  useEffect(esisteGia, [
-    window.location.href,
-    props.location.pathname,
-    account
-  ]);
+  useEffect(esisteGia, [udid]);
 
   function responseGoogle(risposta) {
-    let udid = "109232597291200925390";
+    debugger;
+    console.log(risposta);
+    cambiaUDID(risposta.googleId);
+    console.log(udid);
   }
 
   const stile = {
     marginTop: "-20%"
   };
 
-  return (
-    <React.Fragment>
-      <FullNavBar
-        elementi={[
-          {
-            nome: "Stai attento a quello che carichi, non ti conviene",
-            dati: []
-          }
-        ]}
-      />{" "}
-      <div style={stile}>
-        <Bottone TestoBottone="Per caricare" link="/Login/CaricaRiassunto" />
-        <Bottone TestoBottone="Per approvare" link="/Login/ApprovaRiassunto" />
-        <MostraRiassunti account={account} />
-        <div id="body"> </div>{" "}
-      </div>{" "}
-    </React.Fragment>
-  );
+  $("#loadingImage").fadeOut(500, "swing");
+
+  if (ok) {
+    $("#loadingImage").fadeOut(500, "swing");
+    return (
+      <React.Fragment>
+        <FullNavBar
+          elementi={[
+            {
+              nome: "Stai attento a quello che carichi, non ti conviene",
+              dati: []
+            }
+          ]}
+        />{" "}
+        <div style={stile}>
+          <Bottone TestoBottone="Per caricare" link="/Login/CaricaRiassunto" />
+          <Bottone
+            TestoBottone="Per approvare"
+            link="/Login/ApprovaRiassunto"
+          />
+          <MostraRiassunti account={udid} />
+          <div id="body"> </div>{" "}
+        </div>{" "}
+      </React.Fragment>
+    );
+  } else {
+    $("#loadingImage").fadeOut(500, "swing");
+    return (
+      <React.Fragment>
+        <FullNavBar
+          elementi={[
+            {
+              nome: "Stai attento a quello che carichi, non ti conviene",
+              dati: []
+            }
+          ]}
+        />{" "}
+        <div style={{ marginTop: "-20%" }}>
+          <GoogleLogin
+            clientId="757171675502-tn1k2bjmh123u729uqufjhg0nr8d1br1.apps.googleusercontent.com"
+            buttonText="Login"
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy={"single_host_origin"}
+          />
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Login;
