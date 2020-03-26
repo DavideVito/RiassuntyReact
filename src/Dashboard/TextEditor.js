8afddca1310c4f693315b1dfecbe36070e0f1368
DavideVitiello <lgvitiello9@gmail.com>
2020-03-26 20:44:25 +0100

    Aggiunto il text editor

--- src/Dashboard/TextEditor.js
+++ src/Dashboard/TextEditor.js
@@ -1,48 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
+import $ from "jquery";
 import { Editor } from "@tinymce/tinymce-react";
 
-class MyEditor extends React.Component {
-  handleEditorChange = (content, editor) => {
-    console.log(editor);
+function MyEditor() {
+  let [numeroRighe, cambiaNumeroRighe] = useState(1);
+
+  let handleEditorChange = (content, editor) => {
+    let righe = content.split("\n");
+    if (numeroRighe === righe.length) {
+      return;
+    }
+    cambiaNumeroRighe(righe.length);
+
+    let titolo = $(righe[0]).text();
+    let contenuto = content;
+    let token = sessionStorage.token;
+    let idRiassunto = "";
+    if (typeof sessionStorage.idRiassunto === "undefined") {
+      idRiassunto = "no";
+    } else {
+      idRiassunto = sessionStorage.idRiassunto;
+    }
+    console.log("carico");
+    $.ajax({
+      url:
+        //"https://vps.lellovitiello.tk/Riassunty/API/caricaTestoTemporaneo.php",
+        "http://localhost/~davidevitiello/Riassunty/API/caricaTestoTemporaneo.php",
+      data: {
+        token: token,
+        nome: titolo,
+        contenuto: contenuto,
+        idRiassunto: idRiassunto
+      },
+      method: "POST",
+      success: data => {
+        sessionStorage.idRiassunto = data.idRiassunto;
+      }
+    });
   };
 
-  render() {
-    return (
-      <React.Fragment>
-        <div
-          style={{
-            height: "250px"
-          }}
-        >
-          {" "}
-        </div>
-        <div className="row">
-          <div className="col-md-2"> </div>{" "}
-          <div className="col-md">
-            <Editor
-              textareaName="testoUtente"
-              placeholder="Scrivi qua il tuo riassunto, verrà salvato in automatico"
-              init={{
-                height: 500,
-                menubar: false,
-                plugins: [
-                  "advlist format autolink lists link image charmap print preview anchor",
-                  "searchreplace visualblocks code fullscreen",
-                  "insertdatetime media table paste code help wordcount save"
-                ],
-                toolbar:
-                  "undo redo | formatselect | bold italic forecolor | \
+  return (
+    <React.Fragment>
+      <div
+        style={{
+          height: "250px"
+        }}
+      >
+        {" "}
+      </div>{" "}
+      <div className="row">
+        <div className="col-md-2"> </div>{" "}
+        <div className="col-md">
+          <Editor
+            textareaName="testoUtente"
+            placeholder="Scrivi qua il tuo riassunto, verrà salvato in automatico"
+            init={{
+              height: 500,
+              menubar: false,
+              plugins: [
+                "advlist format autolink lists link image charmap print preview anchor",
+                "searchreplace visualblocks code fullscreen",
+                "insertdatetime media table paste code help wordcount save"
+              ],
+              toolbar:
+                "undo redo | formatselect | bold italic forecolor | \
              alignleft aligncenter alignright alignjustify | \
              bullist numlist outdent indent | help"
-              }}
-              onEditorChange={this.handleEditorChange}
-            />{" "}
-          </div>{" "}
-          <div className="col-md-2"> </div>{" "}
+            }}
+            onEditorChange={handleEditorChange}
+          />{" "}
         </div>{" "}
-      </React.Fragment>
-    );
-  }
+        <div className="col-md-2"> </div>{" "}
+      </div>{" "}
+    </React.Fragment>
+  );
 }
 
 export default MyEditor;
